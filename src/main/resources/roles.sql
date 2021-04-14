09d27e6ccd33e792546c7d624e6a91073a40ad3b
EwKow <82374339+EwKow@users.noreply.github.com>
2021-04-14 12:54:19 +0200

    Add files via upload

---
+++ src/main/resources/roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO `moviesappdb`.`role` (`role_id`, `name`) VALUES ('1', 'USER');
+INSERT INTO `moviesappdb`.`role` (`role_id`, `name`) VALUES ('2', 'ADMIN');
+INSERT INTO `moviesappdb`.`role` (`role_id`, `name`) VALUES ('3', 'MODERATOR');
